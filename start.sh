576fc0c9c5b755f740119b0e51e4e4e4f1f14244
jbfink <john.fink@gmail.com>
2013-08-04 18:21:43 -0400

    Create and test for /mysql-configured for completed mysql configuration in start.sh; thanks to @eugeneware's http://github.com/eugeneware/docker-wordpress-nginx

--- start.sh
+++ start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+if [ ! -f /mysql-configured ]; then
 /usr/bin/mysqld_safe & 
 sleep 10s
 MYSQL_PASSWORD=`pwgen -c -n -1 12`
 echo mysql root password: $MYSQL_PASSWORD
 echo $MYSQL_PASSWORD > /mysql-root-pw.txt
 mysqladmin -u root password $MYSQL_PASSWORD 
+touch /mysql-configured
 killall mysqld
 sleep 10s
+fi
 supervisord -n
